36ce4935157fd7b3d0ad1cbaf3afdd3f2d55de75
raphste <raphael.sterk@johner-institut.de>
2021-11-05 15:31:53 +0100

    update coinflip game: change var scores to val

--- src/main/scala/game/CoinFlipGame.scala
+++ src/main/scala/game/CoinFlipGame.scala
@@ -9,16 +9,19 @@ import de.htwg.rs.chatbot.model.{TwitchInput, TwitchInputParser}
 import scala.collection.MapOps
 import scala.util.{Failure, Success}
 
-class CoinFlipGame(parentInput: TwitchInput, output: TwitchOutput, twitchConnection: TwitchConnection, channel: String) extends TwitchConsumer {
+class CoinFlipGame(parentInput: TwitchInput, output: TwitchOutput, twitchConnection: TwitchConnection, channel: String, playerWins: Int = 0, playerLosses: Int = 0, isNewInstance: Boolean = true) extends TwitchConsumer {
   val parentUserId = parentInput.user.userId
   val parser = new TwitchInputParser()
   val playerName = parentInput.user.displayName
   val parentMessageMap = Map("reply-parent-msg-id" -> parentInput.message.id)
-  var playerWins = 0 //TODO is there a way to track the score without var?
-  var playerLosses = 0
+  //val playerWins = 0 //TODO is there a way to track the score without var?
+  //var playerLosses = 0
 
   twitchConnection.getInput.subscribe(this)
-  output.sendMessage(channel, s"started a new flip a coin game for player ${playerName}! Make your choice: (h)ead or (t)ails. (s)core to see your results. (e)xit to end the game.", parentMessageMap)
+
+  if (isNewInstance) {
+    output.sendMessage(channel, s"started a new flip a coin game for player ${playerName}! Make your choice: (h)ead or (t)ails. (s)core to see your results. (e)xit to end the game.", parentMessageMap)
+  }
 
 
   override def onMessage(message: String): Unit =
@@ -54,10 +57,12 @@ class CoinFlipGame(parentInput: TwitchInput, output: TwitchOutput, twitchConnect
 
   def generateMatchResultMessage(isPlayerWin: Boolean): String =
     if (isPlayerWin)
-      playerWins = playerWins + 1
+      //playerWins = playerWins + 1
+      updateCoinflipGameInstance(playerWins + 1, playerLosses)
       "WutFace WutFace H0w did that happen?! you won! NotLikeThis  Anyway. Congrats Kappa Kappa "
     else
-      playerLosses = playerLosses + 1
+      //playerLosses = playerLosses + 1
+      updateCoinflipGameInstance(playerWins, playerLosses + 1)
       "O neim! du hast verloren!!! lelelelel PogChamp PogChamp PogChamp"
 
   def sendResult(): Unit =
@@ -68,4 +73,8 @@ class CoinFlipGame(parentInput: TwitchInput, output: TwitchOutput, twitchConnect
   def endGame() =
     output.sendMessage(channel, "ok baiii", parentMessageMap)
     twitchConnection.getInput.unSubscribe(this)
+
+  def updateCoinflipGameInstance(playerWins: Int, playerLosses: Int) =
+    twitchConnection.getInput.unSubscribe(this)
+    new CoinFlipGame(parentInput, output, twitchConnection, channel, playerWins, playerLosses, false)
 }
