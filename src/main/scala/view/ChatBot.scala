f3e97b2ee315401925c58ef06e7c73e2465dbe36
raphste <raphael.sterk@johner-institut.de>
2021-11-18 09:45:34 +0100

    some more tinkering with external dsl. chatbot class

--- src/main/scala/view/ChatBot.scala
+++ src/main/scala/view/ChatBot.scala
@@ -4,28 +4,35 @@ package view
 import control.{ChannelOutput, CommandRegistryRegisty, TwitchInputProvider, `when message`}
 import game.CoinFlipGameHandler
 import io.{Config, TwitchConnection}
+import de.htwg.rs.chatbot.control._
 
 object ChatBot {
   def main(args: Array[String]): Unit = {
+
     val bot = Config.bots.head
     val twitchConnection = TwitchConnection.establishConnection(bot.name, bot.auth)
     twitchConnection.start()
     val twitchInput = twitchConnection.getInput
     val twitchOutput = twitchConnection.getOutput
 
+    val registries = new CommandRegistryRegisty(twitchOutput)
+
+    twitchInput.subscribe(new TwitchInputProvider(twitchOutput, twitchConnection, registries))
+
+    args.foreach(twitchConnection.join(_))
+
+
     val heyCommand = `when message` `starts with` "hey" `respond with` "TheIlluminati"
     val helpCommand = `when message` `contains` "help" `respond with` "Commands: hello, help, play"
     val starWars = `when message` `ends with` "hello there" `respond with` "General Kenobi BrainSlug"
     val helloCommand = `when message` `is` "hello" `respond with` "Hello there! HeyGuys HeyGuys"
 
-    val registries = new CommandRegistryRegisty(twitchOutput)
+
     registries.addCommand("imperiabot", heyCommand)
     registries.addCommand("imperiabot", helpCommand)
-    registries.addCommand("imperiabot", starWars)
     registries.addCommand("imperiabot", helloCommand)
     registries.addCommand("imperiabot", new CoinFlipGameHandler())
-
-    twitchInput.subscribe(new TwitchInputProvider(twitchOutput, twitchConnection, registries))
-    args.foreach(twitchConnection.join(_))
+    registries.addCommand("imperiabot", new CreateCommandHandler())
+    registries.addCommand("imperiabot", starWars)
   }
 }
