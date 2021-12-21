b393703bda579d0317af6e0d91b6d39ba83d5d1c
Patrick Kratzer <38780545+pat721@users.noreply.github.com>
2021-12-21 13:45:54 +0100

    Reformat

--- src/main/scala/game/CoinFlipGameHandler.scala
+++ src/main/scala/game/CoinFlipGameHandler.scala
@@ -1,22 +1,18 @@
 package de.htwg.rs.chatbot
 package game
 
+import control.ChannelOutput
 import model.{Command, TwitchInput}
-
-import de.htwg.rs.chatbot.control.ChannelOutput
 
 case class CoinFlipGameHandler(instances: List[CoinFlipGame] = List.empty) extends Command {
   override def handle(input: TwitchInput): (Command, Option[String]) = input.message.text match {
     case "p" | "play" => startGame(input)
     case _ =>
       var response: Option[String] = None
-      val newInstances = instances.map(
-        _.handle(input)
-      )
-        .map { case (game, gameResponse) => {
+      val newInstances = instances.map(_.handle(input))
+        .map { case (game, gameResponse) =>
           gameResponse.foreach(r => response = Some(r))
           game
-        }
         }
         .filter(_.isDefined)
         .map(_.get)
