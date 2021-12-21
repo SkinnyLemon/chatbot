30706a35e679105d0cec02865a187ade77c289a9
Patrick Kratzer <38780545+pat721@users.noreply.github.com>
2021-12-21 14:26:43 +0100

    Update CoinFlipGameHandler.scala

--- src/main/scala/game/CoinFlipGameHandler.scala
+++ src/main/scala/game/CoinFlipGameHandler.scala
@@ -20,6 +20,6 @@ case class CoinFlipGameHandler(instances: List[CoinFlipGame] = List.empty) exten
   }
 
   def startGame(input: TwitchInput): (Command, Option[String]) =
-    val newGame = new CoinFlipGame(input.user)
+    val newGame = CoinFlipGame(input.user)
     (copy(instances = instances :+ newGame), Some(s"started a new flip a coin game for player ${input.user.displayName}! Make your choice: (h)ead or (t)ails. (s)core to see your results. (e)xit to end the game."))
 }
