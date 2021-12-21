ebd28d2dee5094a166e36d59f223fd890d66d624
Patrick Kratzer <38780545+pat721@users.noreply.github.com>
2021-12-21 14:26:52 +0100

    Refactor code

--- src/test/scala/game/CoinFlipGameHandlerTest.scala
+++ src/test/scala/game/CoinFlipGameHandlerTest.scala
@@ -1,7 +1,8 @@
 package de.htwg.rs.chatbot
 package game
 
-import de.htwg.rs.chatbot.model.{Channel, Message, TwitchInput, User}
+import model.*
+
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{times, verify, when}
 import org.scalatest.matchers.should.Matchers
@@ -10,37 +11,39 @@ import org.scalatestplus.mockito.MockitoSugar.mock
 
 class CoinFlipGameHandlerTest extends AnyWordSpec with Matchers {
 
-  val coinFlipGameHandler = CoinFlipGameHandler()
-  val twitchInputMock = mock[TwitchInput]
-  val userObject = User(userName = "TestUser", displayName = "TestUser", userId = "1234")
-  when(twitchInputMock.user).thenReturn(userObject)
-
-
   "A CoinFlipGameHandlerTest object" should {
-    "start a game with play" in {
 
-      val playMessageObject = Message(Array.empty, "play", 123123, "123123")
-      when(twitchInputMock.message).thenReturn(playMessageObject)
+    val twitchInputMock = mock[TwitchInput]
+    val userObject = User(userName = "TestUser", displayName = "TestUser", userId = "1234")
+    when(twitchInputMock.user).thenReturn(userObject)
+
+    "start a game with a play command" in {
+
+      val coinFlipGameHandler = CoinFlipGameHandler()
+      val messageObject = Message(Array.empty, "play", 123123, "123123")
+      when(twitchInputMock.message).thenReturn(messageObject)
 
       coinFlipGameHandler.handle(twitchInputMock)
-      verify(twitchInputMock,times(2)).user
+      verify(twitchInputMock, times(2)).user
     }
-    "copy the existing instances to ignore other messages" in {
+    "ignore other commands" in {
 
       val coinFlipGameMock = mock[CoinFlipGame]
-      val coinFlipGameReal = CoinFlipGame(userObject)
-
       val listCoinFlipMock = List(coinFlipGameMock)
 
-      val coinFlipGameHandler2 = CoinFlipGameHandler(listCoinFlipMock)
+      val coinFlipGameHandler = CoinFlipGameHandler(listCoinFlipMock)
 
+      val messageObject = Message(Array.empty, "no play command", 123456789, "id-1234")
+      when(twitchInputMock.message).thenReturn(messageObject)
 
-      val noPlayMessageObject = Message(Array.empty, "some message", 123123, "123123")
-      when(twitchInputMock.message).thenReturn(noPlayMessageObject)
+      when(coinFlipGameMock.handle(twitchInputMock)).thenReturn((Some(coinFlipGameMock), Some("Testmessage")))
 
-      when(coinFlipGameMock.handle(twitchInputMock)).thenReturn((Some(coinFlipGameMock), Some("hello pls")))
+      coinFlipGameHandler.handle(twitchInputMock) shouldBe a[(Command, Option[String])]
+    }
+    "start a game" in {
 
-      coinFlipGameHandler2.handle(twitchInputMock)
+      val coinFlipGameHandler = CoinFlipGameHandler()
+      coinFlipGameHandler.startGame(twitchInputMock) shouldBe a[(Command, Option[String])]
     }
   }
 
