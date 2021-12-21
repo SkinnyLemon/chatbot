16436c0ecbbc8a93275c88a838cfa7ccde4d1c4d
raphste <raphael.sterk@johner-institut.de>
2021-12-21 10:12:40 +0100

    spark consumer to show words by count

--- src/main/scala/io/SparkKafkaConsumer.scala
+++ src/main/scala/io/SparkKafkaConsumer.scala
@@ -1,16 +1,15 @@
 package de.htwg.rs.chatbot
 package io
 
-import de.htwg.rs.chatbot.model.TwitchInput
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-object SparkKafkaConsumer extends App{
+object SparkKafkaConsumer extends App {
 
   val sparkConfig = SparkSession.builder.appName("SparkKafkaConsumer").config("spark.master", "local[*]").getOrCreate()
   val sparkStreamingContext = new StreamingContext(sparkConfig.sparkContext, Seconds(1))
@@ -32,7 +31,20 @@ object SparkKafkaConsumer extends App{
     Subscribe[String, String](kafkaTopics, kafkaConnectionConfig)
   )
 
-  val records = stream.map(record => record.value().split(".*Emote.*?,")(1).split(".*?,")(0).split(" "))
+  val records = stream.map(record => record.value().split(".*Emote.*?,")(1).split(",.*,")(0).split(" "))
+
+  import sparkConfig.implicits._
+
+  var dfSchema = Array("word")
+  var dataFrame = Seq.empty[String].toDF(dfSchema: _*)
+  dataFrame.createOrReplaceTempView("results")
+
+  records.foreachRDD(rdd => {
+    val newRow = rdd.flatMap(word => word).toDF(dfSchema: _*)
+    dataFrame = dataFrame.union(newRow)
+    dataFrame.groupBy("word").count().sort(desc("count")).show()
+  })
+
 
   sparkStreamingContext.start() // start the computation
   sparkStreamingContext.awaitTermination() // await termination
