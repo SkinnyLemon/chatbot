c1c8994f257191b1e65d0e8b078b7500da6056b6
Patrick Kratzer <38780545+pat721@users.noreply.github.com>
2022-01-22 16:41:23 +0100

    Add tests

---
+++ src/test/scala/ai/hiLo/HiLoClassifierTest.scala
@@ -0,0 +1,36 @@
+package de.htwg.rs.chatbot
+package ai.hiLo
+
+import de.htwg.rs.chatbot.ai.{Classifier, Evaluator}
+import model.*
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar.mock
+
+class HiLoClassifierTest extends AnyWordSpec with Matchers {
+  "A HiLoClassifier object" should {
+
+    "define a multilayernetwork" in {
+      val multiLayerNetwork = HiLoClassifier().defineNetwork()
+
+      multiLayerNetwork shouldBe a [MultiLayerNetwork]
+    }
+
+    "initialize a HiLoGame evaluator object" in {
+      val initializedHiLoClassifier = HiLoClassifier().initialize()
+
+      initializedHiLoClassifier shouldBe a [Evaluator[HiLoGame]]
+    }
+
+    "evaluate a deck" in {
+      val hiLoDeck = HiLoGame(firstNumber = 7, secondNumber = 10, thirdNumber = 11, fourthNumber = 9, fifthNumber = 12)
+      val evaluatedHiLoDeck = HiLoClassifier().initialize().evaluate(hiLoDeck)
+
+      evaluatedHiLoDeck.bestChoice should be (0)
+    }
+  }
+}
---
+++ src/test/scala/ai/iris/IrisClassifierTest.scala
@@ -0,0 +1,51 @@
+package de.htwg.rs.chatbot
+package ai.iris
+
+import de.htwg.rs.chatbot.ai.{Classifier, Evaluator}
+import model.*
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar.mock
+
+class IrisClassifierTest extends AnyWordSpec with Matchers {
+
+  "A IrisClassifier object" should {
+
+    "define a multilayernetwork" in {
+      val multiLayerNetwork = IrisClassifier().defineNetwork()
+
+      multiLayerNetwork shouldBe a [MultiLayerNetwork]
+    }
+
+    "initialize a iris evaluator object" in {
+      val initializedIrisClassifier = IrisClassifier().initialize()
+
+      initializedIrisClassifier shouldBe a [Evaluator[Iris]]
+    }
+
+    "evaluate a iris" in {
+      val iris = Iris(sepalLength =  5.1, sepalWidth = 3.5, petalLength = 1.4, petalWidth = 0.2)
+      val evaluatedIris = IrisClassifier().initialize().evaluate(iris)
+
+      evaluatedIris.irisClass should startWith ("Iris Setosa")
+    }
+
+    "get iris names for classes" in {
+      val irisSetosa = IrisClassifier().getIrisClass(0)
+      irisSetosa should startWith ("Iris Setosa")
+
+      val irisVersicolor = IrisClassifier().getIrisClass(1)
+      irisVersicolor should startWith ("Iris Versicolor")
+
+      val irisVirginica = IrisClassifier().getIrisClass(2)
+      irisVirginica should startWith ("Iris Virginica")
+
+      val notValidInput = IrisClassifier().getIrisClass(3)
+      notValidInput should startWith ("not valid")
+    }
+  }
+}
